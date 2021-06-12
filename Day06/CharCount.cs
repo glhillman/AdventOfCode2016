e98f26c7b4d006ad3fa43773f8598803a49a23f2
glhillman <glhillman@gmail.com>
2021-06-12 08:02:50 -0600

    Initial commit

---
+++ Day06/CharCount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day06
+{
+    class CharCount
+    {
+        public CharCount(char c)
+        {
+            Char = c;
+            Count = 0;
+        }
+
+        public char Char { get; private set; }
+        public int Count { get; set;}
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", Char, Count);
+        }
+    }
+}
---
+++ Day06/Day06.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day06
+{
+    class Day06
+    {
+        List<string> _rawData;
+
+        public Day06()
+        {
+            LoadData();
+        }
+
+        public void Part1()
+        {
+            List<List<CharCount>> charCounts = new List<List<CharCount>>();
+            
+            for (int i=0; i<_rawData[0].Length; i++)
+            {
+                charCounts.Add(CreateCharCount());
+            }
+
+            foreach (string data in _rawData)
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+                    charCounts[i].Find(r => r.Char == data[i]).Count++;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < charCounts.Count; i++)
+            {
+                sb.Append(charCounts[i].OrderByDescending(r => r.Count).First().Char);
+            }
+
+            Console.WriteLine("Part1: {0}", sb.ToString()); ;
+        }
+
+        public void Part2()
+        {
+
+            List<List<CharCount>> charCounts = new List<List<CharCount>>();
+
+            for (int i = 0; i < _rawData[0].Length; i++)
+            {
+                charCounts.Add(CreateCharCount());
+            }
+
+            foreach (string data in _rawData)
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+                    charCounts[i].Find(r => r.Char == data[i]).Count++;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < charCounts.Count; i++)
+            {
+                var x = charCounts[i].OrderBy(r => r.Count);
+                sb.Append(charCounts[i].OrderBy(r => r.Count).First(r => r.Count > 0).Char);
+            }
+
+            Console.WriteLine("Part2: {0}", sb.ToString()); ;
+        }
+
+        List<CharCount> CreateCharCount()
+        {
+            List<CharCount> charCount = new List<CharCount>();
+
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                charCount.Add(new CharCount(c));
+            }
+
+            return charCount;
+        }
+
+        private void LoadData()
+        {
+            string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\input.txt";
+
+            if (File.Exists(inputFile))
+            {
+                _rawData = new List<string>();
+
+                string line;
+                StreamReader file = new StreamReader(inputFile);
+                while ((line = file.ReadLine()) != null)
+                {
+                    _rawData.Add(line);
+                }
+
+                file.Close();
+            }
+        }
+    }
+}

5d7b63a863d4d0f140db26e46d5d66cc9a7d9975
glhillman <glhillman@gmail.com>
2022-01-13 20:44:48 -0700

    Day12

---
+++ Day12/Command.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12
+{
+    enum Cmds
+    {
+        cpy,
+        inc,
+        dec,
+        jnz
+    };
+
+    internal class Command
+    {
+        public Command(Cmds cmd, bool xIsVar, int x = 0, bool yIsVar = false, int y = 0)
+        {
+            // depending on the cmd, x or y may be constants or indexes into the variables array [0..3] = [a, b, c, d]
+            Cmd = cmd;
+            XIsVar = xIsVar;
+            X = x;
+            YIsVar = yIsVar;
+            Y = y;
+        }
+
+        public Cmds Cmd { get; private set; }
+        public bool XIsVar { get; private set; }
+        public int X { get; private set; }
+        public bool YIsVar { get; private set; }
+        public int Y { get; private set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2} {3} {4}", Cmd.ToString(), XIsVar ? "var" : "const", X, YIsVar ? "var" : "const", Y);
+        }
+    }
+}
---
+++ Day12/DayClass.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12
+{
+    internal class DayClass
+    {
+        List<Command> _commands = new();
+        public DayClass()
+        {
+            LoadData();
+        }
+
+        
+        public void Part1()
+        {
+            int[] vars = { 0, 0, 0, 0 };
+
+            Run(_commands, vars);
+
+            Console.WriteLine("Part1: {0}", vars[0]); // a
+        }
+
+        public void Part2()
+        {
+            int[] vars = { 0, 0, 1, 0 };
+
+            Run(_commands, vars);
+
+            Console.WriteLine("Part2: {0}", vars[0]); // a
+        }
+
+        public void Run(List<Command> commands, int[] vars)
+        {
+            int ip = 0;
+            int eofIp = commands.Count;
+
+            while (ip < eofIp)
+            {
+                Command cmd = commands[ip];
+                switch (cmd.Cmd)
+                {
+                    case Cmds.cpy:
+                        vars[cmd.Y] = cmd.XIsVar ? vars[cmd.X] : cmd.X;
+                        ip++;
+                        break;
+                    case Cmds.inc:
+                        vars[cmd.X]++;
+                        ip++;
+                        break;
+                    case Cmds.dec:
+                        vars[cmd.X]--;
+                        ip++;
+                        break;
+                    case Cmds.jnz:
+                        if (vars[cmd.X] != 0)
+                        {
+                            ip += cmd.YIsVar ? vars[cmd.Y] : cmd.Y;
+                        }
+                        else
+                        {
+                            ip++;
+                        }
+                        break;
+                }
+            }
+        }
+        private void LoadData()
+        {
+            string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+            if (File.Exists(inputFile))
+            {
+                string? line;
+                StreamReader file = new StreamReader(inputFile);
+                while ((line = file.ReadLine()) != null)
+                {
+
+                    string[] parts = line.Split(' ');
+                    switch (parts[0])
+                    {
+                        case "cpy":
+                            {
+                                (bool xIsVar, int xValue) = Parse(parts[1]);
+                                (bool yIsVar, int yValue) = Parse(parts[2]);
+                                _commands.Add(new Command(Cmds.cpy, xIsVar, xValue, yIsVar, yValue));
+                            }
+                            break;
+                        case "inc":
+                            {
+                                (bool xIsVar, int xValue) = Parse(parts[1]);
+                                _commands.Add(new Command(Cmds.inc, xIsVar, xValue));
+                            }
+                            break;
+                        case "dec":
+                            {
+                                (bool xIsVar, int xValue) = Parse(parts[1]);
+                                _commands.Add(new Command(Cmds.dec, xIsVar, xValue));
+                            }
+                            break;
+                        case "jnz":
+                            {
+                                (bool xIsVar, int xValue) = Parse(parts[1]);
+                                (bool yIsVar, int yValue) = Parse(parts[2]);
+                                _commands.Add(new Command(Cmds.jnz, xIsVar, xValue, yIsVar, yValue));
+                            }
+                            break;
+                    }
+                    // Process the string
+                }
+
+                file.Close();
+            }
+        }
+
+        private (bool, int) Parse(string token)
+        {
+            bool isVar;
+            int value;
+
+            switch (token)
+            {
+                case "a":
+                case "b":
+                case "c":
+                case "d":
+                    isVar = true;
+                    value = token[0] - 'a';
+                    break;
+                default:
+                    isVar = false;
+                    value = int.Parse(token);
+                    break;
+            }
+
+            return (isVar, value);
+        }
+    }
+}

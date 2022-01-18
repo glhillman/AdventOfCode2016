76212b92d4eae54abad73941d9b235d2210c69da
glhillman <glhillman@gmail.com>
2022-01-18 14:16:40 -0700

    Day14

---
+++ Day14/AoCMD5.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14
+{
+    internal class AoCMD5
+    {
+        public AoCMD5(string seed, int stretch = 0)
+        {
+            Seed = seed;
+            Stretch = stretch;
+
+            Md5 = MD5.Create();
+            Hashes = new();
+        }
+
+        private string Seed { get; set; }
+        private int Stretch { get; set; }
+        private MD5 Md5 { get; set; }
+        Dictionary<int, string> Hashes { get; set; }
+        public string Hash(int key)
+        {
+            string? hash;
+
+            if (Hashes.TryGetValue(key, out hash) == false)
+            {
+                string fullKey = Seed + key.ToString();
+                if (Stretch > 0)
+                {
+                    hash = CreateStretchedMD5(fullKey);
+                }
+                else
+                {
+                    hash = CreateMD5(fullKey);
+                }
+                Hashes[key] = hash;
+            }
+
+            return hash;
+        }
+
+        private string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = Md5.ComputeHash(inputBytes);
+
+            // Convert the byte array to hexadecimal string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+        private string CreateStretchedMD5(string input)
+        {
+            string key = CreateMD5(input);
+            for (int i = 0; i < Stretch; i++)
+            {
+                key = CreateMD5(key);
+            }
+            return key;
+        }
+    }
+}
---
+++ Day14/DayClass.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Day14
+{
+    internal class DayClass
+    {
+        public void Part1()
+        {
+            int nKeys = 0;
+            int i = 0;
+
+            //string baseKey = "abc";
+            string baseKey = "ngcjuoqr";
+
+            AoCMD5 md5 = new AoCMD5(baseKey, 0);
+
+            while (nKeys < 64)
+            {
+                ++i;
+                string hash = md5.Hash(i);
+                char? seq3 = Find3(hash, i);
+                if (seq3 != null)
+                {
+                    bool found5 = false;
+                    for (int j = i+1; j < i + 1000 && found5 == false; j++)
+                    {
+                        string hash2 = md5.Hash(j);
+                        if (Find5(hash2, seq3.Value, j))
+                        {
+                            found5 = true;
+                            //Console.WriteLine("key {0}, i,j = {1},{2} hash1 {3}, hash 2 {4} contains 5 of {5}", nKeys+1, i, j, hash, hash2, seq3.Value);
+                            if (++nKeys == 64)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Part1: {0}", i);
+        }
+
+        public void Part2()
+        {
+            int nKeys = 0;
+            int i = 0;
+            //string baseKey = "abc";
+            string baseKey = "ngcjuoqr";
+            AoCMD5 md5 = new AoCMD5(baseKey, 2016);
+
+            while (nKeys < 64)
+            {
+                ++i;
+                string hash = md5.Hash(i); ;
+                char? seq3 = Find3(hash, i);
+                if (seq3 != null)
+                {
+                    bool found5 = false;
+                    for (int j = i + 1; j < i + 1000 && found5 == false; j++)
+                    {
+                        string hash2 = md5.Hash(j);
+                        if (Find5(hash2, seq3.Value, j))
+                        {
+                            found5 = true;
+                            //Console.WriteLine("key {0}, i,j = {1},{2} hash1 {3}, hash 2 {4} contains 5 of {5}", nKeys + 1, i, j, hash, hash2, seq3.Value);
+                            if (++nKeys == 64)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Part2: {0}", i);
+        }
+
+        private char? Find3(string hash, int index)
+        {
+            char? result = null;
+
+            for (int i = 0; i < hash.Length-2; i++)
+            {
+                if (hash[i] == hash[i+1] && hash[i] == hash[i+2])
+                {
+                    result = hash[i];
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        private bool Find5(string hash, char value, int index)
+        {
+            bool found = false;
+            for (int i = 0; i < hash.Length - 4; i++)
+            {
+                if (hash[i] == value && hash[i + 1] == value && hash[i + 2] == value && hash[i + 3] == value && hash[i + 4] == value)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            return found;
+        }
+    }
+}

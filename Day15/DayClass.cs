adec56f369537dc97ebadc0cd9c1f36fbec91c6d
glhillman <glhillman@gmail.com>
2022-01-18 18:08:33 -0700

    Day15

---
+++ Day15/DayClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day15
+{
+    internal class DayClass
+    {
+        public void Part1()
+        {
+            List<Disc> discs = LoadData();
+
+            int startTime = Align(discs);
+
+            Console.WriteLine("Part1: {0}", startTime);
+        }
+
+        public void Part2()
+        {
+            List<Disc> discs = LoadData();
+            discs.Add(new Disc(discs.Count+1, 11, 0, 0));
+
+            int startTime = Align(discs);
+
+            Console.WriteLine("Part2: {0}", startTime);
+        }
+
+        private int Align(List<Disc> discs)
+        {
+            // get them all 1 second apart
+            for (int i = 0; i < discs.Count; i++)
+            {
+                discs[i].AdjustTime(i + 1);
+            }
+
+            // get the disc with greatest Positions value at position 0, then we can use its Positions value to step
+            Disc disc = discs.First(c => c.Positions == discs.Max(c => c.Positions));
+            int step = disc.Positions - disc.Position;
+            for (int i = 0; i < discs.Count; i++)
+            {
+                discs[i].AdjustTime(step);
+            }
+
+            step = disc.Positions;
+
+            int steps = 1;
+            int sum;
+            do
+            {
+                steps++;
+                sum = 0;
+                for (int i = 0; i < discs.Count; i++)
+                {
+                    sum += discs[i].AdjustTime(step);
+                }
+            }
+            while (sum != 0);
+
+            return discs[0].Time - 1;
+        }
+        private List<Disc> LoadData()
+        {
+            List<Disc> discs = new();
+
+            string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+            if (File.Exists(inputFile))
+            {
+                string? line;
+                StreamReader file = new StreamReader(inputFile);
+                while ((line = file.ReadLine()) != null)
+                {
+                    MatchCollection matches = Regex.Matches(line, "[0-9]+");
+                    discs.Add(new Disc(int.Parse(matches[0].Value), int.Parse(matches[1].Value), int.Parse(matches[2].Value), int.Parse(matches[3].Value)));
+                }
+
+                file.Close();
+            }
+
+            return discs;
+        }
+
+    }
+}
---
+++ Day15/Disc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15
+{
+    internal class Disc
+    {
+        public Disc(int id, int positions, int time, int position)
+        {
+            ID = id;
+            Positions = positions;
+            Time = time;
+            Steps = position;
+            Position = position;
+        }
+
+        public int ID { get; private set; }
+        public int Positions { get; private set; }
+        public int Time { get; set; }
+        public int Position { get; set; }
+        public int Steps { get; set; }
+        public int AdjustTime(int increment)
+        {
+            Time += increment;
+            Steps += increment;
+            Position = Steps % Positions;
+            return Position;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("ID: {0}, NPos: {1}, Time: {2}, Position: {3}", ID, Positions, Time, Position);
+        }
+    }
+}
